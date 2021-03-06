a7e9974df2e71daf2a6ad97172ddea296ce3981d
john vivas <jbyronvivas@gmail.com>
2021-03-06 17:36:48 -0500

    degunda entrega

---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+function NavBar() {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  
+  <a class="navbar-brand" href="#">Logo</a>
+
+  
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 2</a>
+    </li>
+
+   
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Dropdown link
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Link 1</a>
+        <a class="dropdown-item" href="#">Link 2</a>
+        <a class="dropdown-item" href="#">Link 3</a>
+      </div>
+    </li>
+  </ul>
+</nav>
+    </div>
+  );
+}
+
+export default NavBar;
